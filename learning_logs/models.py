01ced64d812beecd84567fdf0bea72575c945f94
stevekutz <stkutz@gmail.com>
2020-02-02 13:26:10 -0600

    added Topic model, updating README

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(model.Model):
+""" Topic that the use in learning about """
+    text = models.CharField(max_length=200)
+    data_added = models.DataTimeField(auto_now_add=True)
+    def __str__(self):
+        """ Return a string representation of the model """
+        return self.text
