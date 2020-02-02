91d33023e1e27e2cab12542fdcbd624e8feac262
stevekutz <stkutz@gmail.com>
2020-02-02 16:06:26 -0600

    created admin accounts and added topics through admin interface

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from learning_logs.models import Topic  # import Topic model to be registered
+admin.site.register(Topic)  # regsters Topic to be managed via admin site 
