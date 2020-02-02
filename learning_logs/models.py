24068cca8d0db491d5fad3cd663ab06301a81e0e
stevekutz <stkutz@gmail.com>
2020-02-02 15:22:12 -0600

    created and ran migrations for Topic model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Topic(model.Model):
-""" Topic that the use in learning about """
-    text = models.CharField(max_length=200)
-    data_added = models.DataTimeField(auto_now_add=True)
+class Topic(models.Model):
+    """ Topic that the use in learning about """
+    text = models.CharField(max_length=200) # allocates 200 chars in database
+    data_added = models.DateTimeField(auto_now_add=True) # sets to current data/time when new topic created
     def __str__(self):
         """ Return a string representation of the model """
         return self.text
