3b75f6f588520ab17781ae30688cb3bdcac83cec
stevekutz <stkutz@gmail.com>
2020-02-07 19:56:52 -0600

    updated README for Entry model registration

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Topic, Entry
+from .models import Topic, Entry    # imports app
 
 # Register your models here.
 from learning_logs.models import Topic  # import Topic model to be registered
-admin.site.register(Topic)  # regsters Topic to be managed via admin site 
+admin.site.register(Topic)  # regsiters Topic to be managed via admin site 
 admin.site.register(Entry)
