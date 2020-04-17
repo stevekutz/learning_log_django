b0827597ad30d957fbb33cef1ddcbc3c1b43f936
stevekutz <stkutz@gmail.com>
2020-04-17 16:58:38 -0500

    created register() function

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 """ Define URL patterns for user """
 
-from django.urls import path, include
+from django.urls import path, include  # for login page
+from . import views   # for registration page
 
 app_name = 'users'
 
 urlpatterns = [
     # Include the default authentication urls
     path('', include('django.contrib.auth.urls')),
+    # Define URL pattern for registration
+    path('register/', views.register, name = 'register'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    """ Regsiter a new user if GET request made """
+    if request.method != 'POST' and request.method == 'GET':
+        # present user a registration form with no data
+        form = UserCreationForm()
+    else:     
+        # process a completed form with POST data
+        form = UserCreationForm(data = request.POST)
+
+        if form.is_valid:
+            new_user = form.save()
+            # Log in user and the redirect to home page
+
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Display blank of invalid form
+    context = { 'form': form }        
+    return render(request, 'registration/register.html', context)
