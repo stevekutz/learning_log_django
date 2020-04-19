de0454423e5b08d5394465c3c6882e15368e0961
stevekutz <stkutz@gmail.com>
2020-04-19 18:44:46 -0500

    added owner field to Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,6 +7,9 @@ class Topic(models.Model): # inherit from Django's base Model class
     """ Topic that the use in learning about """
     text = models.CharField(max_length=200) # allocates 200 chars in database
     data_added = models.DateTimeField(auto_now_add=True) # sets to current data/time when new topic created
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
     def __str__(self):
         """ Return a string representation of the model """
         return self.text
