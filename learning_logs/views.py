5707ce4042fc9e1b3bc6a39c2541abde1442ea22
stevekutz <stkutz@gmail.com>
2020-04-20 19:13:30 -0500

    created functionality to protect data created by specific users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,7 +13,8 @@ def index(request):
 @login_required
 def topics(request):
     """ Show all topics """
-    topics = Topic.objects.order_by('data_added')
+    # topics = Topic.objects.order_by('data_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('data_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -20,6 +22,11 @@ def topics(request):
 def topic(request, topic_id):
     """ Show a single topic and all of its entries""" 
     topic = Topic.objects.get(id=topic_id)
+    
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -34,7 +41,11 @@ def new_topic(request):
         # POST data exists, process data within request.POST
         form = TopicForm(data = request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            
+            # form.save()
             return redirect('learning_logs:topics')
 
     # Display a blank or invalid form
@@ -68,6 +79,10 @@ def edit_entry(request, entry_id):
     """ Edit an exiting entry """
     entry = Entry.objects.get(id = entry_id)
     topic = entry.topic
+
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
 
     if (request.method != 'POST' and request.method == 'GET'):
         # GET request => this is an initial request, pre-fill form with current entry data
